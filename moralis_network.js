fd719289da01bc1a564e5493b1fea8548a12a95e
Nobert Jakpor <jakpornobert@gmail.com>
2022-02-24 17:53:20 +0100

    new

--- moralis_network.js
+++ moralis_network.js
@@ -2,10 +2,16 @@ import Moralis from "moralis/types";
 
 
 function MoralisNetwork() {
-    /* Moralis init code */
-const serverUrl = "https://c8ghhjpwuhc0.usemoralis.com:2053/server";
-const appId = "Y4QYLPMmIPY7LCPEJ9mvdU5sYf0oLcX3xvzmDekP1";
-Moralis.start({ serverUrl, appId });
+        /* Moralis init code */
+    const serverUrl = "https://c8ghhjpwuhc0.usemoralis.com:2053/server";
+    const appId = "Y4QYLPMmIPY7LCPEJ9mvdU5sYf0oLcX3xvzmDekP1";
+    const sdk = Moralis
+    sdk.start({ serverUrl, appId });
 
-/* TODO: Add Moralis Authentication code */
+    /* TODO: Add Moralis Authentication code */
+
+    const auth = async function() {
+        const user_auth = await sdk.authenticate()
+    }
+
 }
