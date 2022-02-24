94125a6b0a37ed4f4062d53df5d4b0332c1416f0
Nobert Jakpor <jakpornobert@gmail.com>
2022-02-24 17:07:53 +0100

    initial commit

---
+++ moralis_network.js
@@ -0,0 +1,11 @@
+import Moralis from "moralis/types";
+
+
+function MoralisNetwork() {
+    /* Moralis init code */
+const serverUrl = "https://c8ghhjpwuhc0.usemoralis.com:2053/server";
+const appId = "Y4QYLPMmIPY7LCPEJ9mvdU5sYf0oLcX3xvzmDekP1";
+Moralis.start({ serverUrl, appId });
+
+/* TODO: Add Moralis Authentication code */
+}
