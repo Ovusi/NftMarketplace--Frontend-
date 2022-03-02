3683df5e15ac7fa0ec59f63ecf7bde67a1ab5c0e
Nobert Jakpor <jakpornobert@gmail.com>
2022-03-02 12:54:15 +0100

    new

--- Lvldb.js
+++ Lvldb.js
@@ -1,4 +1,33 @@
 const level = require("level")
-const db = require("level")
+const levelup = require("../NftMarketplace (Frontend)/node_modules/levelup")
+const leveldown = require("leveldown")
 
-const lvl = level("mydb")
+/*  */
+
+
+function Users() {
+    const db = level("mydb") // Create database store
+
+    this.add = async (key, value) => {
+        const keyString = JSON.stringify(key)
+        const valueString = JSON.stringify(value)
+
+        await db.put(keyString, valueString)
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err))
+    }
+
+    this.retreive = async (key) => {
+        const keyString = JSON.stringify(key)
+        await db.get(keyString)
+            .then((data) => {return console.log(JSON.parse(data))})
+            .catch((err) => console.log("NO"))
+    }
+}
+ddb = new Users()
+ddb.add(22, {num: 44})
+const tt = ddb.retreive(22)
+console.log(tt)
+const v = {num: 40904}
+console.log(v.num)
+
