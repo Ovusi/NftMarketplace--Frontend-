8fb51eb615b098ec54ef7a7149b0800a7dc2b8f6
Nobert Jakpor <jakpornobert@gmail.com>
2022-03-02 13:19:19 +0100

    new

--- Lvldb.js
+++ Lvldb.js
@@ -19,15 +19,35 @@ function Users() {
 
     this.retreive = async (key) => {
         const keyString = JSON.stringify(key)
-        await db.get(keyString)
-            .then((data) => {return console.log(JSON.parse(data))})
+        return await db.get(keyString)
+            .then((data) => console.log(JSON.parse(data)))
             .catch((err) => console.log("NO"))
     }
 }
 ddb = new Users()
-ddb.add(22, {num: 44})
+ddb.add(22, {num: 446})
 const tt = ddb.retreive(22)
-console.log(tt)
+console.log(tt.num)
 const v = {num: 40904}
 console.log(v.num)
 
+
+function ListingIds() {
+    const db = level("mydb") // Create database store
+
+    this.add = async (key, value) => {
+        const keyString = JSON.stringify(key)
+        const valueString = JSON.stringify(value)
+
+        await db.put(keyString, valueString)
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err))
+    }
+
+    this.retreive = async (key) => {
+        const keyString = JSON.stringify(key)
+        await db.get(keyString)
+            .then((data) => console.log(JSON.parse(data)))
+            .catch((err) => console.log("NO"))
+    }
+}
