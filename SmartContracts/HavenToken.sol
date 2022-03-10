cd489f01fce57603c04a491220dc64b389b14795
Nobert Jakpor <jakpornobert@gmail.com>
2022-03-10 18:53:33 +0100

    new

--- SmartContracts/HavenToken.sol
+++ SmartContracts/HavenToken.sol
@@ -5,30 +5,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HavenToken is ERC20 {
-    uint256 private _totalSupply = 100000000;
+    uint256 private _totalSupply = 100000000 * 10 ** decimals();
 
     string private _name = "Haven Token";
     string private _symbol = "HVX";
 
     constructor() ERC20(_name, _symbol) {
         _mint(msg.sender, _totalSupply);
-    }
-}
-
-
-contract HelloWorld {
-    string public greeting = "Hello World"; // state variable
-
-
-    constructor(string memory greet) { // constructor
-        greeting = greet; 
-    }
-
-    function helloWorld(string memory newGreeting) public {
-        greeting = newGreeting;
-    }
-
-    function newHello(string memory update) public {
-        greeting = update;
     }
 }
