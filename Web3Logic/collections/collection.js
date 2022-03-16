8f5fa00b81d8d33665e2fc0cbedbc49380ec2454
Nobert Jakpor <jakpornobert@gmail.com>
2022-03-16 22:50:30 +0100

    new

--- Web3Logic/collections/collection.js
+++ Web3Logic/collections/collection.js
@@ -2,25 +2,30 @@ const { Web3 } = require('web3');
 const { ABI, BYTE_CODE } = require("../collections/data")
 const { Component } = require("/../Users/Nobert Jakpor/Desktop/NftMarketplace (Frontend)/component")
 
-async function DeployCollection() {
+async function DeployCollection(name, symbol) {
     const comp = Component()
-    const rpcURL = "http://127.0.0.1:7545"
+    const rpcURL = comp.provider
     const web3 = new Web3(rpcURL)
 
     const deploy_contract = new web3.eth.Contract(JSON.parse(ABI))
 
     const payload = {
-        data: BYTE_CODE
+        data: BYTE_CODE,
+        arguments: web3.eth.abi.encodeParameters(["address", "address", "string", "string"], [nftmarketaddress, senderadd, name, symbol])
     }
 
     const parameter = {
         from: comp.address
     }
 
-    await deploy_contract.deploy(payload).send(parameter, (err, transactionHash) => {
-        console.log('Transaction Hash :', transactionHash);
-    }).on('confirmation', () => { }).then((newContractInstance) => {
-        console.log('Deployed Contract Address : ', newContractInstance.options.address);
-    })
+    this.deploy_collection = async () => {
+        await deploy_contract.deploy(payload).send(parameter, (err, transactionHash) => {
+            console.log('Transaction Hash :', transactionHash);
+        }).on('confirmation', () => { }).then((newContractInstance) => {
+            console.log('Deployed Contract Address : ', newContractInstance.options.address);
+        })
+
+        return newContractInstance.options.address
+    }
 
 }
