b21dde9b523632f45d9df8c9064f8cb4d2a8df1a
Nobert Jakpor <jakpornobert@gmail.com>
2022-03-17 11:17:46 +0100

    new

--- web3storage/webstorage.js
+++ web3storage/webstorage.js
@@ -1,7 +1,7 @@
 const process = require('process')
 const minimist = require('minimist')
 const { Web3Storage, getFilesFromPath } = require('web3.storage')
-const IPFS = require('ipfs-mini')
+const { create } = require('ipfs-http-client')
 const internal = require('stream')
 
 
