d5adce0e2bd54a813d3b717057dcc75c17cf5593
Nobert Jakpor <jakpornobert@gmail.com>
2022-05-09 16:59:46 +0100

    new

--- Web3Logic/component.js
+++ Web3Logic/component.js
@@ -1,16 +1,16 @@
 const { useWeb3, useSwitchNetwork, ThirdwebWeb3Provider } = require("@3rdweb/hooks")
 const { Web3 } = require('web3');
 
-function ConnectWallet() {
+async function ConnectWallet() {
     // This is the CUSTOM function that enables the connect wallet button in the dapp
     // You can do whatever you want with this returned data
     try {
         const web3 = new Web3(Web3.givenProvider || "provider_")
-        const account = web3.eth.getAccounts()[0]
-        const provider = web3.eth.currentProvider()
-        const chainId = web3.eth.getChainId()
-        const balance = web3.eth.getBalance()
-        const accounts = web3.eth.requestAccounts()
+        const account = await web3.eth.getAccounts()[0]
+        const provider = await web3.eth.currentProvider()
+        const chainId = await web3.eth.getChainId()
+        const balance = await web3.eth.getBalance()
+        const accounts = await web3.eth.requestAccounts()
         
         return {
             account,
