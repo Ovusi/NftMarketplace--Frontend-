81d28ed08ebd7ab6c14acbb704d3f635b33da0d8
Nobert Jakpor <jakpornobert@gmail.com>
2022-05-11 00:22:12 +0100

    new

--- Web3Logic/component.js
+++ Web3Logic/component.js
@@ -1,5 +1,6 @@
 const { useWeb3, useSwitchNetwork, ThirdwebWeb3Provider } = require("@3rdweb/hooks")
 const { Web3 } = require('web3');
+import detectEthereumProvider from '@metamask/detect-provider';
 
 async function ConnectWallet() {
     // This is the CUSTOM function that enables the connect wallet button in the dapp
@@ -11,7 +12,7 @@ async function ConnectWallet() {
         const chainId = await web3.eth.getChainId()
         const balance = await web3.eth.getBalance(account)
         const accounts = await web3.eth.requestAccounts()
-        
+
         return {
             account,
             provider,
@@ -19,9 +20,35 @@ async function ConnectWallet() {
             balance,
             accounts,
         }
-    } catch {}
-    
+    } catch { }
 
+
+}
+
+async function ConnectMetamask() {
+    // This is the CUSTOM function that enables the connect wallet button in the dapp
+    // You can do whatever you want with this returned data
+
+    if (typeof ethereum.isMetaMask() != true) {
+        console.log('MetaMask is not installed!');
+    } else {
+        accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+            .then((data) => {
+                if (data) {
+                    const account = accounts[0]
+                    const provider = await detectEthereumProvider()
+                    const chainIds = await ethereum.request({ method: 'eth_chainId' })
+                    const chainId = chainIds[0]
+
+                    return {
+                        account,
+                        provider,
+                        chainId
+                    }
+                }
+            })
+            .catch((err) => {})
+    }
 }
 
 
