ecf9b256ce2f4f8a2d66b0795f4a0e7170b4c239
Nobert Jakpor <jakpornobert@gmail.com>
2022-05-11 00:49:32 +0100

    new

--- Web3Logic/component.js
+++ Web3Logic/component.js
@@ -5,22 +5,31 @@ import detectEthereumProvider from '@metamask/detect-provider';
 async function ConnectWallet() {
     // This is the CUSTOM function that enables the connect wallet button in the dapp
     // You can do whatever you want with this returned data
-    try {
-        const web3 = new Web3(Web3.givenProvider || "provider_")
-        const account = await web3.eth.getAccounts()[0]
-        const provider = await web3.eth.currentProvider()
-        const chainId = await web3.eth.getChainId()
-        const balance = await web3.eth.getBalance(account)
-        const accounts = await web3.eth.requestAccounts()
 
-        return {
-            account,
-            provider,
-            chainId,
-            balance,
-            accounts,
-        }
-    } catch { }
+    const web3 = await new Web3(window.ethereum)
+        .then((data) => {
+            if (data) {
+                const accounts = await web3.eth.getAccounts()
+                const account = accounts[0]
+                const provider = await web3.eth.currentProvider()
+                const chainId = await web3.eth.getChainId()
+                const balance = await web3.eth.getBalance(account)
+
+                return {
+                    account,
+                    provider,
+                    chainId,
+                    balance,
+                }
+            }
+        })
+        .catch((err) => {
+            if (typeof ethereum.isMetaMask() != true) {
+                console.log('MetaMask is not installed!');
+            } else {
+                console.log(err)
+            }
+        })
 
 
 }
@@ -47,7 +56,7 @@ async function ConnectMetamask() {
                     }
                 }
             })
-            .catch((err) => {})
+            .catch((err) => { })
     }
 }
 
