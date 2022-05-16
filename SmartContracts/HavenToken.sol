f7bf5bc5418202384b679ccb1e110d5fe513b158
Nobert Jakpor <jakpornobert@gmail.com>
2022-05-16 13:49:49 +0100

    new

--- SmartContracts/HavenToken.sol
+++ SmartContracts/HavenToken.sol
@@ -6,13 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HavenToken is ERC20 {
     uint256 private stakeAllocation = 10000000;
+    uint256 private maxSupply = 100000000;
     uint256 private initialMintSupply = 90000000;
-    uint256 private _totalSupply = 100000000 * 10**decimals();
 
     string private _name = "Haven Token";
     string private _symbol = "HVX";
 
     constructor() ERC20(_name, _symbol) {
-        _mint(msg.sender, _totalSupply);
+        _mint(msg.sender, initialMintSupply * 10**decimals());
     }
 }
