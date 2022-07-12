a223af1288c503aa32f724d3f54b8ef06ac4c36c
Nobert Jakpor <jakpornobert@gmail.com>
2022-07-12 09:52:51 +0100

    new

--- SmartContracts/libs.sol
+++ SmartContracts/libs.sol
@@ -7,69 +7,45 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
-contract Payments {
+library Payments {
     using SafeMath for uint256;
 
-    function buy(
+    address constant tokenContract_ =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address private constant MATIC = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address private constant HVXTOKEN =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+        
+
+    function payment(
         address nftContract,
         address seller,
         address currency,
-        address[] memory payees,
         uint256 tokenId,
         uint256 amount
     ) internal {
-
-
-        uint256 fee = (amount * 2) / 100;
-        uint256 commision = amount - fee;
-        uint256 payeeNumber = payees.length;
-        uint256 payeeCommission = fee / payeeNumber;
-
-        IERC721(nftContract).transferFrom(
-            address(this),
-            msg.sender,
-            tokenId
-        );
-        IERC20(currency).transferFrom(msg.sender, seller, commision);
-        for (uint256 receiver = 0; receiver <= payeeNumber; receiver++) {
-            IERC20(currency).transferFrom(
+        if (currency == MATIC || currency == HVXTOKEN) {
+            IERC721(nftContract).transferFrom(
                 address(this),
-                payees[receiver],
-                payeeCommission
+                msg.sender,
+                tokenId
             );
+            IERC20(currency).transferFrom(msg.sender, address(this), amount); // Todo
+
+        } else if (currency == HVXTOKEN) {
+            IERC721(nftContract).transferFrom(
+                address(this),
+                msg.sender,
+                tokenId
+            );
+            IERC20(currency).transferFrom(address(this), seller, commision); // Todo
+            IERC20(tokenContract_).transferFrom(
+                address(this),
+                tokenContract_,
+                fee
+            ); // Todo
+        } else {
+            revert();
         }
     }
-
-}
-
-
-contract UserLib {
-    enum verified {
-        yes,
-        no
-    }
-    struct User {
-    verified verified;
-    address userAddress;
-    uint256 regTime;
-    string userURI;
-    address[] ownedCollections;
-    }
-
-    mapping(address => User) users_;
-    mapping(address => address[]) private ownedCollections_;
-
-    function addNewUser(string memory useruri_) internal {
-        User storage userr = users_[msg.sender];
-        require(msg.sender != userr.userAddress, "Not Authorized!");
-        User memory user = User(
-            verified.no,
-            msg.sender,
-            block.timestamp,
-            useruri_,
-            ownedCollections_[msg.sender]
-        );
-        users_[msg.sender] = user;
-    }
-
 }
