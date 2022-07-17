7eb0997c4f08a9b6b38eab1a4bfb0614728b0ff3
Nobert Jakpor <jakpornobert@gmail.com>
2022-07-17 12:14:52 +0100

    new

--- Web3Logic/pinataipfs/pinata.js
+++ Web3Logic/pinataipfs/pinata.js
@@ -6,16 +6,14 @@ function PinataConnect(apiKey, secreteApiKey) {
 
   const isAuthenticated = async () => {
     await pinata.testAuthentication().then((result) => {
-      //handle successful authentication here
       return result.authenticated
     }).catch((err) => {
-      //handle error here
       return err
     })
   }
 
   this.pinFile = async (imageURI, metadata) => {
-    if (isAuthenticated() == true)
+    if (isAuthenticated() === true)
       await pinata.pinFileToIPFS(imageURI, metadata).then((result) => {
         return result
       }).catch((err) => {
